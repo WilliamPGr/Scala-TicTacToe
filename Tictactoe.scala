0361a768aa1a2f321fd03b5da9fdac9dc3bceddc
William B17 <76631826+William-B17@users.noreply.github.com>
2022-08-12 12:34:36 +0200

    Update Tictactoe.scala

--- Tictactoe.scala
+++ Tictactoe.scala
@@ -1,23 +1,33 @@
-@main def mainfunction(): Unit = {
-        val colors = scala.collection.mutable.Map("turn" -> "x", "a1" -> " ", "a2" -> " ","a3" -> " ", "b1" -> " ", "b2" -> " ","b3" -> " ", "c1" -> " ", "c2" -> " ","c3" -> " ")
-        def tick(): Unit = {
-            if (colors("turn") == "x") {
-                colors("turn") = "o" 
-            } else {
-                colors("turn") = "x" 
+
+object Tictactoe {
+    def main(args: Array[String]) = {
+            val colors = scala.collection.mutable.Map("turn" -> "x", "a1" -> " ", "a2" -> " ","a3" -> " ", "b1" -> " ", "b2" -> " ","b3" -> " ", "c1" -> " ", "c2" -> " ","c3" -> " ")
+            def tick(): Unit = {
+                if (colors("turn") == "x") {
+                    colors("turn") = "o" 
+                } else {
+                    colors("turn") = "x" 
+                }
             }
-        }
-        def printboard(): Unit = {
-            print("a1 [" + colors("a1") + "] " + "a2 [" + colors("a2") + "] " + "a3 [" + colors("a3") + "]\n" + "b1 [" + colors("b1") + "] " + "b2 [" + colors("b2") + "] " + "b3 [" + colors("b3") + "]\n" + "c1 [" + colors("c1") + "] " + "c2 [" + colors("c2") + "] " + "c3 [" + colors("c3") + "]\n")
-        }
-        def main(): Unit = {
-            print("Choose placement: ")
-            val inputmain = scala.io.StdIn.readLine()
-            colors(inputmain) = colors("turn")
-            tick()
+            def check(varx:String): Unit = {
+                def won(): Unit = {
+                    println("Someone Won!")
+                }
+                def iff(y:string)
+            }
+            def printboard(): Unit = {
+                print("a1 [" + colors("a1") + "] " + "a2 [" + colors("a2") + "] " + "a3 [" + colors("a3") + "]\n" + "b1 [" + colors("b1") + "] " + "b2 [" + colors("b2") + "] " + "b3 [" + colors("b3") + "]\n" + "c1 [" + colors("c1") + "] " + "c2 [" + colors("c2") + "] " + "c3 [" + colors("c3") + "]\n")
+            }
+            def xmain(): Unit = {
+                print("Choose placement: ")
+                val inputmain = scala.io.StdIn.readLine()
+                colors(inputmain) = colors("turn")
+                tick()
+                printboard()
+                check()
+                xmain()
+            }
             printboard()
-            main()
-        }
-        printboard()
-        main()
+            xmain()
+    }
 }
