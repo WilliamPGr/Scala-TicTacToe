a7ea85ad973bde91cd1c493ec29284b171a9cd26
William B17 <76631826+William-B17@users.noreply.github.com>
2022-08-24 10:29:44 +0200

    Update Tictactoe.scala

--- Tictactoe.scala
+++ Tictactoe.scala
@@ -9,11 +9,17 @@ object Tictactoe {
                     colors("turn") = "x" 
                 }
             }
-            def check(varx:String): Unit = {
+            def check(x:String): Unit = {
                 def won(): Unit = {
-                    println("Someone Won!")
+                    tick()
+                    println(colors("turn") + " won!")
                 }
-                def iff(y:string)
+                def iff(y:String, y1:String, y2:String, y3:String, y4:String) = {
+                    println(y + y1 + y2 + y3 + y4)
+                    if()
+                    won()
+                }
+                iff(x,"1","2","3","4")
             }
             def printboard(): Unit = {
                 print("a1 [" + colors("a1") + "] " + "a2 [" + colors("a2") + "] " + "a3 [" + colors("a3") + "]\n" + "b1 [" + colors("b1") + "] " + "b2 [" + colors("b2") + "] " + "b3 [" + colors("b3") + "]\n" + "c1 [" + colors("c1") + "] " + "c2 [" + colors("c2") + "] " + "c3 [" + colors("c3") + "]\n")
@@ -24,7 +30,7 @@ object Tictactoe {
                 colors(inputmain) = colors("turn")
                 tick()
                 printboard()
-                check()
+                check(inputmain)
                 xmain()
             }
             printboard()
